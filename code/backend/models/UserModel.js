153a01c623eb025b8d42db317a6f95b370e11062
ashishthesatan <ashishthesatan@gmail.com>
2018-07-11 18:34:13 +0545

    Added UserModel Object

---
+++ code/backend/models/UserModel.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let UserSchema = new Schema({
+    fName: String,
+    lName: String,
+    userName: String,
+    password: String,
+    phone: String,
+    city: String,
+    country: String,
+    userType: String
+});
+
+let UserModel = mongoose.model('UserModel',UserSchema);
+
+module.exports = UserModel;
+
