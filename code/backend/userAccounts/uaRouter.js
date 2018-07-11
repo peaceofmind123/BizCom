55c44670def9d7e097e145e8c01902f535d5092d
ashishthesatan <ashishthesatan@gmail.com>
2018-07-11 18:16:23 +0545

    modified aescrypt.java to match the aes-256-cbc standard used in node

--- code/backend/userAccounts/uaRouter.js
+++ code/backend/userAccounts/uaRouter.js
@@ -4,6 +4,7 @@ const uaRouter = express.Router();
 //define uaRouter get, post, put methods here
 uaRouter.route('/signup').post((req,res)=>{
     // the req.body object now contains the object sent from the client
+    console.log(req.body);
     res.send("Success")
 
 
--- code/frontend/bizcom/app/src/main/java/bizcom/bizcom/AESCrypt.java
+++ code/frontend/bizcom/app/src/main/java/bizcom/bizcom/AESCrypt.java
@@ -2,26 +2,30 @@ package bizcom.bizcom;
 
 import android.util.Base64;
 
+import java.io.UnsupportedEncodingException;
 import java.security.Key;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.AlgorithmParameterSpec;
 
 import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
-
 
 //The AEScrypt algorithm for hashing passwords to be sent across the network
 public class AESCrypt
 {
-    private static final String ALGORITHM = "AES";
+    private static final String ALGORITHM = "AES/CBC/PKCS5Padding";
     private static final String KEY = "1Hcfh667adfDDJ57"; //the private key
-
+    private static final String IV = "4e5Wa71fYoT7MFEZ";
     public static String encrypt(String value) throws Exception
     {
         Key key = generateKey();
         Cipher cipher = Cipher.getInstance(AESCrypt.ALGORITHM);
-        cipher.init(Cipher.ENCRYPT_MODE, key);
+        cipher.init(Cipher.ENCRYPT_MODE, generateKey(), generateIV());
         byte [] encryptedByteValue = cipher.doFinal(value.getBytes("utf-8"));
-        String encryptedValue64 = Base64.encodeToString(encryptedByteValue, Base64.DEFAULT);
-        return encryptedValue64;
+        String encryptedValue = Base64.encodeToString(encryptedByteValue, Base64.DEFAULT);
+        return encryptedValue;
 
     }
 
@@ -37,9 +41,30 @@ public class AESCrypt
 
     }
 
-    private static Key generateKey() throws Exception
+    private static Key generateKey()
     {
-        Key key = new SecretKeySpec(AESCrypt.KEY.getBytes(),AESCrypt.ALGORITHM);
-        return key;
+        try
+        {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] key = md.digest(IV.getBytes());
+            return new SecretKeySpec(key,"AES");
+        }
+        catch (NoSuchAlgorithmException e)
+        {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+    private static AlgorithmParameterSpec generateIV(){
+        try
+        {
+            return new IvParameterSpec(AESCrypt.IV.getBytes("utf-8"));
+        }
+        catch (UnsupportedEncodingException e)
+        {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
